97d469c58c0cc1fb5dbc8a6a85c7c41d6e9fb216
Caleb Payan <caleb.payan@dev.sperantus.com>
2024-09-14 17:12:20 -0600

    Fonts missing in the index.css file added

--- project-client/src/App.tsx
+++ project-client/src/App.tsx
@@ -1,19 +1,21 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import { AuroraBackground } from "./components/ui/aurora-background";
+import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
     <HashRouter>
-      <div className="w-full h-screen overflow-y-auto">
-        <AuroraBackground
-          children={
+      <AuroraBackground
+        children={
+          <div className="w-full h-screen overflow-y-auto relative pt-[100px]">
+            <Navbar />
             <Routes>
               <Route path="/" element={<Home />} />
             </Routes>
-          }
-        />
-      </div>
+          </div>
+        }
+      />
     </HashRouter>
   );
 }
--- project-client/src/components/common/Navbar.tsx
+++ project-client/src/components/common/Navbar.tsx
@@ -1,5 +1,14 @@
 function Navbar() {
-  return <div className="w-full"></div>;
+  return (
+    <div className="w-full h-[100px] fixed top-0 flex justify-between items-center px-10">
+      <span className="text-2xl font-gopherBold text-white">NAME</span>
+      <span className="flex flex-col gap-3 items-end">
+        <div className="w-[30px] bg-white h-[1px]"></div>
+        <div className="w-[25px] bg-white h-[1px]"></div>
+        <div className="w-[30px] bg-white h-[1px]"></div>
+      </span>
+    </div>
+  );
 }
 
 export default Navbar;
